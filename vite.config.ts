fd29722fff94e395bedc746fe282c56c6e057fab
MECTRON <dan200575@gmail.com>
2025-12-12 15:55:02 +0200

    feat: added icons

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
                 {
                     src: 'src/manifest.json',
                     dest: '.'
+                },
+                {
+                    src: 'src/icons',
+                    dest: '.'
                 }
             ]
         })
